97004ec69f9fe9cb8cdd368cb10700bb59e5dfb4
Victor Sumner <victor.sumner@shopify.com>
2022-06-07 21:39:49 -0400

    Modify react-graphql to support apollo v3

--- packages/react-graphql/src/hooks/tests/query.test.tsx
+++ packages/react-graphql/src/hooks/tests/query.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import {DocumentNode} from 'graphql-typed';
-import {ApolloClient, NetworkStatus} from 'apollo-client';
+import {ApolloClient, NetworkStatus} from '@apollo/client';
 import {ApolloLink} from 'apollo-link';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {createGraphQLFactory} from '@shopify/graphql-testing';
